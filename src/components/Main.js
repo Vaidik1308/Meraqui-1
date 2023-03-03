0d28a76acced4fedf751b345ea8662693ce14442
Aryan6919 <113785954+Aryan6919@users.noreply.github.com>
2023-03-03 20:45:39 +0530

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+import MainPart1 from './MainPart1'
+import MainPart2 from './MainPart2'
+import MainPart3 from './MainPart3'
+
+const Main = () => {
+  return (
+    <main className=' w-full'>
+      <video 
+      
+      controlsList='hidden'
+        autoPlay
+        loop
+        muted
+      
+        className='h-[100vh] w-full mt-2 ' 
+        src={require('../image/whyChooseUs/header-1.mp4')} 
+        alt="vide_error"
+      />
+      <MainPart1/>
+      <MainPart2/> 
+      <MainPart3/> 
+    </main>
+  )
+}
+
+export default Main
