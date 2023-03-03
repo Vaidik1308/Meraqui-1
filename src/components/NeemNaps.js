05108206701a16a614276068b5a552c180ba16ec
Aryan6919 <113785954+Aryan6919@users.noreply.github.com>
2023-03-04 00:56:37 +0530

    Add files via upload

---
+++ src/components/NeemNaps.js
@@ -0,0 +1,179 @@
+import React from 'react'
+
+const NeemNaps = () => {
+  return (
+    <main className='sm:w-[100%] sm:mx-auto'>
+    <section className='sm:mt-32 sm:flex sm:w-[90%] sm:mx-auto'>
+      
+          <div className="sm:justify-center mt-12 sm:mt-0 sm:mb-8 sm:w-[65%] sm:flex sm:flex-col">
+                <div className=' ml-2  flex flex-col sm:justify-between sm:h-[30vh] '>
+                    <h1 className=' font-quicksand font-[400] text-4xl text-left sm:text-5xl w-[75%] sm:w-[75%] sm:leading-[1.4] sm:mb-8'>
+                    NAPS AND NATS
+                    </h1>
+                    <p className='text-left sm:w-[98%] mt-4 sm:mt-0 font-poppins font-[300]'>
+                      NAPS
+                    (NAPS is a scheme of the Government of India to promote apprenticeship. Apprenticeship Training consists of Basic Training and On-The-Job Training at the workplace in the industry. Meraqui helps to promote apprenticeship training and increase the engagement of apprentices so that they could later join the mainstream workforce.)
+                    <br></br>
+                    <br></br>
+                    NATS
+                    (National Apprenticeship Training Scheme offers an opportunity for students to get trained in some of the best organizations in the Central, State and Private Sector.The National Apprenticeship Training Scheme in India is a one year programme equipping technically qualified youth with practical knowledge and skills required in their field of work)
+                    </p>
+                </div>
+          </div>
+          <div className='sm:relative sm:right-[-15%] sm:w-[90%]'>
+              <img className='w-[100%] h-auto' src="https://images.unsplash.com/photo-1585713181935-d5f622cc2415?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="" />
+          </div>
+        
+    </section>
+    <section className='w-full my-12 sm:mt-20'>
+    <div className='w-full mb-4 sm:mb-16'>
+      <h2 className='text-3xl sm:text-4xl font-quicksand font-[400]'>About service</h2>
+    </div>
+  
+    <div style={{backgroundImage:"url(https://images.unsplash.com/photo-1531545514256-b1400bc00f31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80)"}} className='bg-cover bg-center w-full sm:h-[155vh]'>
+      <div className='  flex flex-col w-full bg-black bg-opacity-[0.7] text-white sm:justify-center sm:h-[155vh]'>
+        {/* <div className='w-[95%] sm:w-[89%] mx-auto my-4'>
+          <h3 className='text-2xl sm:text-3xl text-left text-cyan-100  font-poppins font-[300]'>
+          Our Philosophy
+          </h3>
+        </div>
+        <div className='w-[95%] sm:w-[89%] mx-auto my-4'>
+          <p className='text-left sm:w-[85%] font-poppins font-[300]'>
+          •	To Facilitate registration of Establishment, Basic Training Centre, or mapping of Basic Training Provider
+•	Facilitate employers in setting up the curriculum, assessment, and certification for apprentices in optional trades
+•	Comply with the provisions of the Apprentices Act 1961 and applicable rules made there under
+•	MERAQUI will furnish all the compliance, returns, etc. on the portal - site on behalf of the organization
+•	MERAQUI will also comply with the formalities required for trade apprentices to appear in All India Trade Tests (NCVT) for Apprentices as and when required
+•	MERAQUI would be processing all the compliances, and grievances according to
+•	Apprentices Act 1961
+•	Processing and Payment of stipends to apprentices 
+
+
+          </p>
+        </div> */}
+        <div className='w-[95%] sm:w-[89%] mx-auto my-4 h-[1000vh]'>
+          <p className='text-left sm:w-[85%] font-poppins font-[300]'>
+            <br></br>
+            <br></br>
+          Meraqui conducts apprenticeship program under NAPS & NATS scheme. Our training programs aim to fill the skill gap while also developing high-caliber staff. The Implementation of Apprenticeship Act 1961 is an obligation to every organization with more than 30 employees.
+
+
+          </p>
+          <br></br>
+          {/* <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'> */}
+          
+          <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'>
+          NAPS
+          
+          </h3>
+          <br></br>
+          <p className='text-left sm:w-[85%] font-poppins font-[300]'>
+          NAPS is a scheme launched by the Government of India to promote apprenticeship. Apprenticeship Training consists of Basic Training and On-The-Job Training at the workplace in the industry. Meraqui helps to promote apprenticeship training and increase the engagement of apprentices so that they could later join the mainstream workforce.
+          <br></br>
+          <br></br>
+          It has been observed that organizations face issues like charting annual apprenticeship plans, mobilization of eligible candidates under designated and optional trades, determining the applicability of Basic Training, and registering or aligning Basic Training Providers.
+          <br></br>
+          <br></br>
+          To simplify and facilitate this process, MSDE has authorized outsourcing the activities like mobilization of candidates, registration, basic training, and other related administrative activities to third parties.
+          <br></br>
+          <br></br>
+          After outsourcing these activities, organizations can concentrate more on their core function and need not engage additional manpower for these activities
+          <br></br>
+          <br></br>
+          
+
+          </p>
+
+          <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'>
+          MERAQUI AS THIRD PARTY AGGREGATOR
+          
+          </h3>
+          <br></br>
+          {/* </h3> */}
+          {/* <p className='text-left sm:w-[85%] font-poppins font-[300]'> */}
+          <p className='text-left sm:w-[85%] font-poppins font-[300]'>
+          •	To Facilitate registration of Establishment, Basic Training Centre, or mapping of Basic Training Provider
+          <br></br>
+•	Facilitate employers in setting up the curriculum, assessment, and certification for apprentices in optional trades
+<br></br>
+•	Comply with the provisions of the Apprentices Act 1961 and applicable rules made there under
+<br></br>
+•	MERAQUI will furnish all the compliance, returns, etc. on the portal - site on behalf of the organization
+<br></br>
+•	MERAQUI will also comply with the formalities required for trade apprentices to appear in All India Trade Tests (NCVT) for Apprentices as and when required
+<br></br>
+•	MERAQUI would be processing all the compliances, and grievances according to
+<br></br>
+•	Apprentices Act 1961
+<br></br>
+•	Processing and Payment of stipends to apprentices 
+<br></br>
+•	Assist in liaise & co-ordination with Apprentice advisor, Apprentice board, or any other related Government Organisation on behalf of the organization
+<br></br>
+
+
+
+
+          </p>
+          <br></br>
+
+
+          {/* </p> */}
+          <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'>
+
+{/* <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'>
+<h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'> */}
+
+{/* <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'> */}
+BENEFITS TO YOUR ORGANIZATION
+
+</h3>
+<br></br>
+
+{/* </h3> */}
+{/* </h3>
+</h3> */}
+<p className='text-left sm:w-[85%] font-poppins font-[300]'>
+          {/* <p className='text-left sm:w-[85%] font-poppins font-[300]'> */}
+          {/* <p className='text-left sm:w-[85%] font-poppins font-[300]'>
+          <p className='text-left sm:w-[85%] font-poppins font-[300]'> */}
+          •	Address the skill gap for the establishment
+          <br></br>
+•	Freedom to design and run apprenticeships in
+<br></br>
+•	Customized courses
+<br></br>
+•	Build talent pipelines
+<br></br>
+•	Reduce attrition
+<br></br>
+•	Reduce expenditure on the recruitment process
+<br></br>
+•	No statutory compliances and exemption from Labour law
+<br></br>
+
+
+
+
+          </p>
+          <br></br>
+
+
+          {/* </p> */}
+
+
+          {/* </p>
+
+
+          </p> */}
+
+
+        </div>
+      </div>
+    </div>
+  </section>
+  </main>
+  )
+}
+
+export default NeemNaps
