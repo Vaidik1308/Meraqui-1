c27c742b87063d6da344d71a424def1f39341603
Aryan6919 <113785954+Aryan6919@users.noreply.github.com>
2023-03-08 00:46:10 +0530

    Add files via upload

---
+++ src/components/Workforce.js
@@ -0,0 +1,132 @@
+import React from 'react'
+import G from '../image/whyChooseUs/GIG.jpg'
+
+const Workforce = () => {
+  return (
+    <main className='sm:w-[100%] sm:mx-auto'>
+    <section className='sm:mt-32 sm:flex sm:w-[90%] sm:mx-auto'>
+      
+          <div className="sm:justify-center mt-12 sm:mt-0 sm:mb-8 sm:w-[65%] sm:flex sm:flex-col">
+                <div className=' ml-2  flex flex-col sm:justify-between sm:h-[30vh] '>
+                    <h1 className='font-quicksand font-[400] text-4xl text-left sm:text-5xl w-[100%] sm:w-[100%] leading-[1.3] sm:leading-[1.4] sm:mb-8'>
+                    Workforce
+                    Management (GIG)
+                    </h1>
+                    <p className='text-left sm:w-[98%] mt-4 sm:mt-0 font-poppins font-[300]'>
+                    Project management involves managing and supervising the transfer of goods from manufacturing to retail to customers or business partners. Meraqui meticulously satisfies client requirements, schedules and manages a company's or organization's supply chain management.
+
+
+            
+                    </p>
+                </div>
+          </div>
+          <div className='sm:relative sm:right-[-15%] sm:w-[90%]'>
+              <img className='w-[100%] h-auto' src={G} alt="" />
+          </div>
+        
+    </section>
+    <section className='w-full my-12 sm:mt-20'>
+    <div className='w-full mb-4 sm:mb-16'>
+      <h2 className='text-3xl sm:text-4xl font-quicksand font-[400]'>About service</h2>
+    </div>
+  
+    <div style={{backgroundImage:"url(https://images.unsplash.com/photo-1600880292089-90a7e086ee0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80)"}} className='bg-cover bg-center w-full sm:h-[100vh]'>
+      <div className='  flex flex-col w-full bg-black bg-opacity-[0.7] text-white sm:h-[100vh] sm:justify-center'>
+        {/* <div className='w-[95%] sm:w-[89%] mx-auto my-4'>
+          <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'>
+          
+          </h3>
+        </div>
+        <div className='w-[95%] sm:w-[89%] mx-auto my-4'>
+          <p className='text-left sm:w-[85%] font-poppins font-[300]'>
+          Our effective Workforce Management reduces expenses while increasing production. It enhances flexibility and aids in the development of a robust, scalable firm. Improving flexibility and planning will lead to more production and, eventually, higher revenues. 
+The primary purpose of workforce management is to keep labour expenditures under control by estimating how many workers you will require on a daily, monthly, or seasonal basis.
+Meraqui also excels in providing on-demand gig workforce for your business with no minimum time commitment. 
+
+
+          </p>
+        </div> */}
+        <div className='w-[95%] sm:w-[89%] mx-auto my-4'>
+          <p className='text-left sm:w-[85%] font-poppins font-[300]'>
+          Workforce management may help you remain on top of the day-to-day, whether your workforce is at a warehouse or manufacturing facility, on a construction site working in the field, or on the road. 
+
+
+          </p>
+          <br></br>
+          {/* <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'> */}
+
+          <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'>
+          Our Process
+          
+          </h3>
+          <br></br>
+          {/* </h3> */}
+          {/* <p className='text-left sm:w-[85%] font-poppins font-[300]'> */}
+          <p className='text-left sm:w-[85%] font-poppins font-[300]'>
+          Meraqui does this through human resource management, which includes recruiting, staffing, training, remuneration, and benefits.
+          <br></br>
+          <br></br>
+          We integrate into your existing business and run alongside you, handling your day-to-day workforce needs so you can focus on what you do best... managing your business.
+          <br></br>
+          <br></br>
+          We also discuss performance management, employee engagement, absence reduction, and other topics. 
+          <br></br>
+          <br></br>
+
+
+          </p>
+          <br></br>
+
+
+          {/* </p> */}
+          <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'>
+
+{/* <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'>
+<h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'> */}
+
+{/* <h3 className='text-2xl sm:text-3xl text-left text-cyan-100 font-poppins font-[300]'> */}
+Why Meraqui?
+
+</h3>
+<br></br>
+
+{/* </h3> */}
+{/* </h3>
+</h3> */}
+<p className='text-left sm:w-[85%] font-poppins font-[300]'>
+          {/* <p className='text-left sm:w-[85%] font-poppins font-[300]'> */}
+          {/* <p className='text-left sm:w-[85%] font-poppins font-[300]'>
+          <p className='text-left sm:w-[85%] font-poppins font-[300]'> */}
+          Our effective Workforce Management reduces expenses while increasing production. It enhances flexibility and aids in the development of a robust, scalable firm. Improving flexibility and planning will lead to more production and, eventually, higher revenues. 
+          <br></br>
+          <br></br>
+          The primary purpose of workforce management is to keep labour expenditures under control by estimating how many workers you will require on a daily, monthly, or seasonal basis.
+          <br></br>
+          <br></br>
+          Meraqui also excels in providing on-demand gig workforce for your business with no minimum time commitment. 
+          <br></br>
+          <br></br>
+
+
+          </p>
+          <br></br>
+
+
+          {/* </p> */}
+
+
+          {/* </p>
+
+
+          </p> */}
+
+
+        </div>
+      </div>
+    </div>
+  </section>
+  </main>
+  )
+}
+
+export default Workforce
